2969ba8bfe05d4410d57d16c41ec56105a688390
Prathmesh Desai <d.prathmesh@gmail.com>
2017-05-09 12:00:33 -0400

    update solution to reflect instructions

--- solution/models/models.js
+++ solution/models/models.js
@@ -1,15 +1,20 @@
 var mongoose = require('mongoose');
 var findOrCreate = require('mongoose-findorcreate');
+
+// Create a connect.js inside the models/ directory that
+// exports your MongoDB URI!
 var connect = process.env.MONGODB_URI || require('./connect');
+
+// If you're getting an error here, it's probably because
+// your connect string is not defined or incorrect.
 mongoose.connect(connect);
 
-// Step 1: Create and edit contacts
+// Step 1: Write your schemas here!
 // Remember: schemas are like your blueprint, and models
 // are like your building!
 var contactSchema = mongoose.Schema({
   name: String,
   phone: String,
-  email: String,
   owner: String
 });
 
@@ -17,7 +22,10 @@ var userSchema = mongoose.Schema({
   username: String,
   password: String,
   phone: String,
-  facebookId: String
+  facebookId: String,
+  facebookToken: String,
+  pictureURL: String,
+  friends: Object
 });
 userSchema.plugin(findOrCreate);
 
@@ -26,11 +34,17 @@ var messageSchema = mongoose.Schema({
   content: String,
   user: String,
   contact: String,
-  channel: String
+  channel: String,
+  status: String,
+  from: String
 });
 
-module.exports = {
-    Contact: mongoose.model('Contact', contactSchema),
-    User: mongoose.model('User', userSchema),
-    Message: mongoose.model('Message', messageSchema)
+// Step 2: Create all of your models here, as properties.
+var models = {
+  Contact: mongoose.model('Contact', contactSchema),
+  User: mongoose.model('User', userSchema),
+  Message: mongoose.model('Message', messageSchema)
 };
+
+// Step 3: Export your models object
+module.exports = models;
--- solution/routes/index.js
+++ solution/routes/index.js
@@ -1,28 +1,30 @@
 var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+
 var accountSid = process.env.ACCOUNT_SID;
 var authToken = process.env.AUTH_TOKEN;
 var fromPhone = process.env.FROM_PHONE;
-var fromEmail = process.env.FROM_EMAIL;
 var twilio = require('twilio')(accountSid, authToken);
-var sendgrid = require('sendgrid');
-var router = express.Router();
-var multer  = require('multer');
-var upload = multer();
+
 var models = require('../models/models');
+
 var Contact = models.Contact;
 var Message = models.Message;
 var User = models.User;
 
 router.post('/messages/receive', function(req, res, next) {
-  User.findOne({phone: fromPhone.substring(2)}, function(err, user) {
+  User.findOne({phone: req.body.To.substring(2)}, function(err, user) {
     Contact.findOne({phone: req.body.From.substring(2)}, function(err, contact) {
-      console.log(user, contact);
       if(err) return next(err);
       var message = new Message({
         created: new Date(),
         content: req.body.Body,
-        user: user._id,
-        contact: contact._id
+        user: user.id,
+        contact: contact.id,
+        from: req.body.From,
+        status: 'Received',
+        channel: 'SMS'
       });
       message.save(function(err) {
         if(err) return next(err);
@@ -30,13 +32,6 @@ router.post('/messages/receive', function(req, res, next) {
       });
     })
   });
-});
-
-router.post('/messages/receive_email', upload.array(), function(req, res, next) {
-  console.log("Got email: " + JSON.stringify(req.body));
-  res.send("Thanks sendgrid <3");
-
-  // TODO: matching users and contacts using email from: and to: is hard =\
 });
 
 router.use(function(req, res, next){
@@ -47,22 +42,40 @@ router.use(function(req, res, next){
   }
 });
 
-/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/contacts');
+});
+
 router.get('/contacts', function(req, res, next) {
   // Load all contacts (that this user has permission to view).
-  Contact.find(function(err, contacts) {
-    if (err) return next(err);
-    res.render('contacts', {
-      contacts: contacts
+  User.findById(req.user, function(err, user) {
+    Contact.find({owner: req.user.id}, function(err, contacts) {
+      if (err) return next(err);
+      res.render('contacts', {
+        contacts: contacts,
+        user: user
+      });
     });
-  });
+  })
 });
 
 router.get('/contacts/new', function(req, res, next) {
   res.render('editContact');
 });
 
-router.get('/contacts/:id', function(req, res) {
+router.post('/contacts/new', function(req, res, next) {
+  var contact = new Contact({
+    name: req.body.name,
+    phone: req.body.phone,
+    owner: req.user.id
+  });
+  contact.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/contacts');
+  })
+});
+
+router.get('/contacts/:id', function(req, res, next) {
   Contact.findById(req.params.id, function(err, contact) {
     if (err) return next(err);
     res.render('editContact', {
@@ -71,23 +84,12 @@ router.get('/contacts/:id', function(req, res) {
   });
 });
 
-router.post('/contacts/new', function(req, res, next) {
-  var contact = new Contact({
-    name: req.body.name,
-    phone: req.body.phone,
-    email: req.body.email
-  });
-  contact.save(function(err) {
-    if (err) return next(err);
-    res.redirect('/contacts');
-  })
-});
-
 router.post('/contacts/:id', function(req, res, next) {
   Contact.findById(req.params.id, function(err, contact) {
     if (err) return next(err);
     contact.name = req.body.name;
     contact.phone = req.body.phone;
+    contact.owner = req.user.id;
     contact.save(function(err) {
       if (err) return next(err);
       res.redirect('/contacts');
@@ -96,26 +98,39 @@ router.post('/contacts/:id', function(req, res, next) {
 });
 
 router.get('/messages', function(req, res, next) {
-  Message.find({user: req.user._id}, function(err, messages) {
+  Message
+  .find({user: req.user.id})
+  .populate({
+    path: 'contact',
+    model: 'Contact'
+  })
+  .exec((err, messages) => {
     if (err) return next(err);
+    messages.map((msg => (msg.formattedDate = moment(msg.created).format("dddd, MMMM Do YYYY, h:mm:ss a"))));
     res.render('messages', {
       messages: messages
     });
-  });
+  })
 });
 
 router.get('/messages/:id', function(req, res, next) {
   Contact.findById(req.params.id, function(err, contact) {
     if (err) return next(err);
-    Message.find({user: req.user._id, contact: req.params.id}, function(err, messages) {
+    Message
+    .find({user: req.user.id, contact: req.params.id})
+    .populate({
+      path: 'contact',
+      model: 'Contact'
+    })
+    .exec((err, messages) => {
       if (err) return next(err);
+      messages.map((msg => (msg.formattedDate = moment(msg.created).format("dddd, MMMM Do YYYY, h:mm:ss a"))));
       res.render('messages', {
         messages: messages,
         contact: contact
       });
     });
   });
-
 });
 
 router.get('/messages/send/:id', function(req, res, next) {
@@ -134,50 +149,23 @@ router.post('/messages/send/:id', function(req, res, next) {
       created: new Date(),
       content: req.body.message,
       user: req.user._id,
-      contact: contact._id
+      contact: contact._id,
+      channel: 'SMS',
+      status: 'Sent',
+      from: ''
     });
 
-    // Check the channel
-    if (req.body.channel==="email") {
-      var
-        helper = sendgrid.mail,
-        from_email = new helper.Email(fromEmail),
-        to_email = new helper.Email(contact.email),
-        subject = "New message from Double Message",
-        content = new helper.Content("text/plain", req.body.message),
-        mail = new helper.Mail(from_email, subject, to_email, content);
-
-      var sg = sendgrid.SendGrid(process.env.SENDGRID_API_KEY);
-      var requestBody = mail.toJSON();
-      var request = sg.emptyRequest();
-      request.method = 'POST';
-      request.path = '/v3/mail/send';
-      request.body = requestBody;
-      sg.API(request, function (response) {
-        console.log(response.statusCode);
-        console.log(response.body);
-        console.log(response.headers);
-        message.channel = "email";
-        message.save(function(err) {
-          if(err) return next(err);
-          res.redirect('/messages/' + req.params.id)
-        });
+    twilio.messages.create({
+      to: "+1" + contact.phone,
+      from: fromPhone,
+      body: req.body.message
+    }, function(err, msg) {
+      if (err) return next(err);
+      message.save(function(err) {
+        if(err) return next(err);
+        res.redirect('/messages/' + req.params.id)
       });
-    }
-    else {
-      twilio.messages.create({
-        to: "+1" + contact.phone,
-        from: fromPhone,
-        body: req.body.message
-      }, function(err, msg) {
-        if (err) return next(err);
-        message.channel = "sms";
-        message.save(function(err) {
-          if(err) return next(err);
-          res.redirect('/messages/' + req.params.id)
-        });
       })
-    }
   });
 });
 
